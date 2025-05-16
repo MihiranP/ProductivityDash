bd4243ba31a3c4aff6181eff0cf69763e92d2611
MihiranP <mihiranpandey360@gmail.com>
2025-05-16 00:19:21 -0500

    added working database class

--- app/backend/data/database.py
+++ app/backend/data/database.py
@@ -1,9 +1,26 @@
-from pydantic import BaseModel
 from config.config import app_settings
-from utils import logger
+from utils.logger import app_logger
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
 
 
-class Database(BaseModel):
-    url: str = (
-        f"postgresql://{app_settings.db_user}:{app_settings.db_password}@{app_settings.db_host}:{app_settings.db_port}/{app_settings.db_name}"
-    )
+class Database:
+    def __init__(self):
+        url = f"postgresql://{app_settings.Config.db_user}:{app_settings.Config.db_password}@{app_settings.Config.db_host}:{app_settings.Config.db_port}/{app_settings.Config.db_name}"
+        self.engine = create_engine(url)
+        self.SessionLocal = sessionmaker(
+            autocommit=False, autoflush=False, bind=self.engine
+        )
+
+    def get_db(self):
+        db = self.SessionLocal()
+        try:
+            app_logger.info("Opening database connection...")
+            yield db
+            app_logger.info("Database connection yielded...")
+        finally:
+            app_logger.info("Closing database connection...")
+            db.close()
+
+
+db = Database()
